5b81daef4b8f3f99f5fc5a008a2f192df51e7b07
Wesley St. John <wesleystjohn@yahoo.com>
2025-06-12 11:00:39 -0700

    blah

--- SuperSoccerTests/DataManager/SwiftData/SwiftDataManagerTests.swift
+++ SuperSoccerTests/DataManager/SwiftData/SwiftDataManagerTests.swift
@@ -75,36 +75,6 @@ struct SwiftDataManagerTests {
         cancellable.cancel()
     }
     
-//    @Test func testDeleteTeams() async throws {
-//        // Arrange
-//        let mockStorage = MockSwiftDataStorage()
-//        let team1 = SDTeam.make(info: .make(city: "1"))
-//        let team2 = SDTeam.make(info: .make(city: "2"))
-//        let team3 = SDTeam.make(info: .make(city: "3"))
-//        mockStorage.mockTeams = [team1, team2, team3]
-//        
-//        let manager = SwiftDataManager(storage: mockStorage)
-//        
-//        // Act
-//        manager.deleteTeams(at: IndexSet([1])) // Delete "2"
-//        
-//        // Assert
-//        #expect(mockStorage.mockTeams.count == 2)
-//        #expect(mockStorage.mockTeams[0].info.city == "1")
-//        #expect(mockStorage.mockTeams[1].info.city == "3")
-//        
-//        var receivedTeams: [Team] = []
-//        let cancellable = manager.teamPublisher.sink { teams in
-//            receivedTeams = teams
-//        }
-//        
-//        #expect(receivedTeams.count == 2)
-//        #expect(receivedTeams[0].info.city == "1")
-//        #expect(receivedTeams[1].info.city == "3")
-//        
-//        cancellable.cancel()
-//    }
-    
     @Test func testTeamPublisherUpdatesWhenTeamsChange() async throws {
         // Arrange
         let mockStorage = MockSwiftDataStorage()
@@ -120,11 +90,7 @@ struct SwiftDataManagerTests {
         #expect(receivedTeamsSequence[0].isEmpty)
         
         // Act 1: Add a team
-        let team1 = Team.make(
-            info: TeamInfo.make(city: "Chicago", teamName: "Bulls"),
-            players: []
-        )
-        manager.addNewTeam(team1)
+        
         
         // Assert 1
         #expect(receivedTeamsSequence.count == 2)
@@ -132,24 +98,12 @@ struct SwiftDataManagerTests {
         #expect(receivedTeamsSequence[1][0].info.city == "Chicago")
         
         // Act 2: Add another team
-        let team2 = Team.make(
-            info: TeamInfo.make(city: "Los Angeles", teamName: "Lakers"),
-            players: []
-        )
-        manager.addNewTeam(team2)
+        
         
         // Assert 2
         #expect(receivedTeamsSequence.count == 3)
         #expect(receivedTeamsSequence[2].count == 2)
         #expect(receivedTeamsSequence[2][1].info.city == "Los Angeles")
-        
-//        // Act 3: Delete a team
-//        manager.deleteTeams(at: IndexSet([0])) // Delete Chicago Bulls
-//        
-//        // Assert 3
-//        #expect(receivedTeamsSequence.count == 4)
-//        #expect(receivedTeamsSequence[3].count == 1)
-//        #expect(receivedTeamsSequence[3][0].info.city == "Los Angeles")
         
         cancellable.cancel()
     }
